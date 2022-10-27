ad5c329b77a69a16b011d1e151a9947837e4827f
Priyanka301198 <116259467+Priyanka301198@users.noreply.github.com>
2022-10-27 19:08:38 +0530

    Update Interfacing.c

--- Interfacing.c
+++ Interfacing.c
@@ -1,119 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-
-#include <linux/can.h>
-#include <linux/can/raw.h>
-
-#include <errno.h>
-
+#include <fcntl.h>  
 #include <termios.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 
-//#include <pthread.h>
-
-int gps() {
-  int serial_port;  
-  char dat,buff[100],GGA_code[3];
-  unsigned char IsitGGAstring=0;
-  unsigned char GGA_index=0;
-  unsigned char is_GGA_received_completely = 0;
-  
-  if ((serial_port = Open ("/dev/ttyUSB0", 9600)) < 0)		/* open serial port */
-  {
-    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
-
-  while(1){
-	  
-		if(serialDataAvail (serial_port) )		/* check for any data available on serial port */
-		  { 
-			dat = tcgetattr(serial_port);		/* receive character serially */		
-			if(dat == '$'){
-				IsitGGAstring = 0;
-				GGA_index = 0;
-			}
-			else if(IsitGGAstring ==1){
-				buff[GGA_index++] = dat;
-				if(dat=='\r')
-					is_GGA_received_completely = 1;
-				}
-			else if(GGA_code[0]=='G' && GGA_code[1]=='G' && GGA_code[2]=='A'){
-				IsitGGAstring = 1;
-				GGA_code[0]= 0; 
-				GGA_code[0]= 0;
-				GGA_code[0]= 0;		
-				}
-			else{
-				GGA_code[0] = GGA_code[1];
-				GGA_code[1] = GGA_code[2];
-				GGA_code[2] = dat;
-				}
-		  }
-		if(is_GGA_received_completely==1){
-			printf("GGA: %s",buff);
-			 is_GGA_received_completely= 0;
-		}
-	}
-	return 0;
-}
-
-int vcan()
+void sleep_ms(int);
+int main(void)
 {
-	int s, i; 
-	int nbyte;
-	struct sockaddr_can addr;
-	struct ifreq ifr;
-	struct can_frame frame;
-
-	printf("CAN Sockets Receive Demo\r\n");
-
-	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {    
-		perror("Socket");
-		return 1;
-	}
-
-	strcpy(ifr.ifr_name, "vcan0" );
-	ioctl(s, SIOCGIFINDEX, &ifr);    
-	memset(&addr, 0, sizeof(addr));
-	addr.can_family = AF_CAN;
-	addr.can_ifindex = ifr.ifr_ifindex;
-
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {  
-		perror("Bind");
-		return 1;
-	}
-
-	nbyte = read(s, &frame, sizeof(struct can_frame));   
-
- 	if (nbyte < 0) {
-		perror("Read");
-		return 1;
-	}
-
-	printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
-
-	for (i = 0; i < frame.can_dlc; i++)
-		printf("%02X ",frame.data[i]);
-
-	printf("\r\n");
-
-	if (close(s) < 0) {
-		perror("Close");
-		return 1;
-	}
-	return 0;
+int fd,i;
+if((fd = open("/dev/ttyUSB0",O_RDWR | O_NOCTTY | O_NDELAY) < 0);
+  {
+    fprintf(stdout, "Unable to open serial device: %s\n", strerror(errno));
+    return 1;
+  }
+for(i = 0; i < 500; i++)
+ {
+  putchar(tcgetattr(fd, &SerialPortSettings));
+  fflush(stdout);
+ }
+fprintf(stdout, "\n");
+Close(fd);
+return 0;
+}
+void sleep_ms(int milliseconds) 
+{
+ struct timespec ts;
+ ts.tv_sec = milliseconds / 1000;
+ ts.tv_nsec = (milliseconds % 1000) * 1000000;
+ nanosleep(&ts, NULL);
 }
 
-int main (){
-     //pthread_t newthread;
-     //pthread_create(&newthread, gps);
-     gps();
-     vcan();
-}
 
